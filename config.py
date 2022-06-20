c2d7f24c247c271a10ba7d6be45172780251a9bc
pxnx <loool1912@web.de>
2022-06-20 11:14:27 +0200

    more user-friendly maybe

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-GROUP = "MNChat"  # "dosisdeutsch" #
+api_id = 1234
+api_hash = 'replace this'
+
+GROUP = "username of group"
+
+strict = False
+
+testing = True
--- main.py
+++ main.py
@@ -1,32 +1,54 @@
 import asyncio
 import datetime
 
+import requests
 from telethon import TelegramClient
 from telethon.tl.functions.channels import EditBannedRequest
-from telethon.tl.types import ChatBannedRights, UserStatusLastMonth
+from telethon.tl.types import ChatBannedRights, UserStatusLastMonth, UserStatusEmpty
 
 import config
 
-api_id = 1234
-api_hash = 'get it at https://api.telegram.org'
+
+async def check_cas(user_id: int):
+    response = requests.get(f"https://api.cas.chat/check?user_id={user_id}")
+    print(user_id, response.json())
+    return response.json()["ok"]
+
+
+async def is_scam(user):
+    is_cas_banned = await check_cas(user.id)
+
+    return is_cas_banned or user.fake or user.scam
+
+
+def is_inactive(user):
+    statuses = [UserStatusEmpty]
+
+    if config.strict:
+        statuses += UserStatusLastMonth
+
+    return user.deleted or type(user.status) in statuses or (user.status is None and not user.bot)
 
 
 async def clear_chat(client):
     deleted_accounts = 0
     async for user in client.iter_participants(config.GROUP):
         print(vars(user))
-        print(user.status)
-        if user.deleted or type(user.status) is UserStatusLastMonth or user.fake or user.scam:
-            print("try kicking...")
+
+        if await is_scam(user) or is_inactive(user):
+            print("try kicking...\n")
             try:
                 deleted_accounts += 1
-                await client(EditBannedRequest(config.GROUP, user, ChatBannedRights(
-                    until_date=datetime.timedelta(minutes=1),
-                    view_messages=True
-                )))
-                await asyncio.sleep(0.5)
+
+                if not config.testing:
+                    await client(EditBannedRequest(config.GROUP, user, ChatBannedRights(
+                        until_date=datetime.timedelta(minutes=1),
+                        view_messages=True
+                    )))
+
             except Exception as exc:
                 print(f"Failed to kick one deleted account because: {str(exc)}")
+        await asyncio.sleep(0.5)
 
     if deleted_accounts:
         print(f"Kicked {deleted_accounts} Deleted Accounts")
@@ -35,5 +57,5 @@ async def clear_chat(client):
 
 
 if __name__ == "__main__":
-    with TelegramClient("remove_inactive", api_id, api_hash) as tgclient:
+    with TelegramClient("remove_inactive", config.api_id, config.api_hash) as tgclient:
         asyncio.get_event_loop().run_until_complete(clear_chat(tgclient))
